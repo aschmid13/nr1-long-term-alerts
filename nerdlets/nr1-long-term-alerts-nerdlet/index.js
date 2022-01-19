cc8bf0e8b6495af5cf0e2d2ca89e4f896b18afc9
Nima <nadib@newrelic.com>
2022-01-19 12:46:07 -0700

    continued working on the fields needed. drop downs update now

--- nerdlets/nr1-long-term-alerts-nerdlet/index.js
+++ nerdlets/nr1-long-term-alerts-nerdlet/index.js
@@ -10,6 +10,9 @@ import {
   HeadingText,
   Select,
   SelectItem,
+  Stack,
+  StackItem,
+  Switch
 } from "nr1";
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
@@ -21,24 +24,33 @@ export default class Nr1LongTermAlertsNerdletNerdlet extends React.Component {
     this.state = { accountId: null };
 
     this.onChangeAccount = this.onChangeAccount.bind(this);
+
+    this._onChange = this._onChange.bind(this);
   }
 
   onChangeAccount(_, value) {
-    alert(`Selected account: ${value}`);
-
     this.setState({ accountId: value });
   }
 
+  _onChange(event, value) {
+    this.setState({ value });
+  }
+
   render() {
+    let styles = {
+      marginRight: "20px",
+      marginLeft: "20px",
+      marginTop: "20px",
+    };
     return (
-      <div>
+      <div style={styles}>
         <Form
           layoutType={Form.LAYOUT_TYPE.SPLIT}
           splitSizeType={Form.SPLIT_SIZE_TYPE.LARGE}
         >
           <TextField
-            placeholder="will be used for your alert name"
             label="Name"
+            description="This name will be used for your alert name, and the underlying Events and Synthetic checks that will be setup"
           />
           <AccountPicker
             label="Account"
@@ -48,8 +60,11 @@ export default class Nr1LongTermAlertsNerdletNerdlet extends React.Component {
           />
           <MultilineTextField
             label="Description"
-            info="Info tooltip"
-            placeholder="Placeholder text"
+            placeholder="Optional"
+          />
+          <MultilineTextField
+            label="NRQL"
+            description="Paste your NRQL query here. You can remove the SINCE statement from your query as you will have option further below"
           />
           <TextField
             placeholder="1 Day Ago"
@@ -60,19 +75,45 @@ export default class Nr1LongTermAlertsNerdletNerdlet extends React.Component {
           <Select
             label="Critical"
             info="Info value"
-            onChange={(evt, value) => alert(value)}
+            onChange={this._onChange}
+            value={this.state.value}
           >
-            <SelectItem value="Above">above</SelectItem>
-            <SelectItem value="Below">below</SelectItem>
-            <SelectItem value="Equals">equals</SelectItem>
+            <SelectItem value="ABOVE">above</SelectItem>
+            <SelectItem value="BELOW">below</SelectItem>
+            <SelectItem value="EQUALS">equals</SelectItem>
           </Select>
-          <TextField placeholder="threshold"/>
-          <Select
-            onChange={(evt, value) => alert(value)}
-          >
+          <TextField placeholder="threshold" />
+          <Select onChange={this._onChange} value={this.state.value}>
             <SelectItem value="ALL">for at least</SelectItem>
             <SelectItem value="AT_LEAST_ONCE">At least once in</SelectItem>
-          </Select> 
+          </Select>
+          <TextField placeholder="15" />
+          <Select onChange={(evt, value) => alert(value)}>
+            <SelectItem value="MINUTES">minutes</SelectItem>
+            <SelectItem value="SECONDS">seconds</SelectItem>
+          </Select>
+            <Switch label="Add a Warning Threshold" 
+            onChange={(e) => alert(`Toggle to: ${e.target.checked}`)} />
+          <Select
+            label="Warning"
+            info="Info value"
+            onChange={this._onChange}
+            value={this.state.value}
+          >
+            <SelectItem value="ABOVE">above</SelectItem>
+            <SelectItem value="BELOW">below</SelectItem>
+            <SelectItem value="EQUALS">equals</SelectItem>
+          </Select>
+          <TextField placeholder="threshold" />
+          <Select onChange={this._onChange} value={this.state.value}>
+            <SelectItem value="ALL">for at least</SelectItem>
+            <SelectItem value="AT_LEAST_ONCE">At least once in</SelectItem>
+          </Select>
+          <TextField placeholder="15" />
+          <Select onChange={(evt, value) => alert(value)}>
+            <SelectItem value="MINUTES">minutes</SelectItem>
+            <SelectItem value="SECONDS">seconds</SelectItem>
+          </Select>
         </Form>
       </div>
     );
