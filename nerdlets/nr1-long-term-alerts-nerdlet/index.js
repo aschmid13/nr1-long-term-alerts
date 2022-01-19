f6144fb1d0616398a795f5ed03478bf193a40c64
Nima <nadib@newrelic.com>
2022-01-19 09:00:01 -0700

    nerdlet framework setup - empty

---
+++ nerdlets/nr1-long-term-alerts-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class Nr1LongTermAlertsNerdletNerdlet extends React.Component {
+  render() {
+    return <h1>Hello, nr1-long-term-alerts-nerdlet Nerdlet!</h1>;
+  }
+}
