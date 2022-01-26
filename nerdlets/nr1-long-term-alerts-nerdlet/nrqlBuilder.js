ee2c63f682b150eb0de75ce5a7b3a4e946344a52
Nima <nadib@newrelic.com>
2022-01-26 13:08:23 -0700

    working on NRQL Builder

--- nerdlets/nr1-long-term-alerts-nerdlet/nrqlBuilder.js
+++ nerdlets/nr1-long-term-alerts-nerdlet/nrqlBuilder.js
@@ -1,44 +1,89 @@
 import react, { Component } from "react";
-import { Stack, StackItem, TextField, MultilineTextField, LineChart, AreaChart, BlockText } from "nr1";
+import {
+  Stack,
+  StackItem,
+  TextField,
+  MultilineTextField,
+  LineChart,
+  AreaChart,
+  BlockText,
+} from "nr1";
 
-//maybe this would be better as a straight up function instead of this. the function creates the NRQL Object and returns that plus a string. 
-
+//maybe this would be better as a straight up function instead of this. the function creates the NRQL Object and returns that plus a string.
 
 class NRQLBuilder extends Component {
   constructor() {
     super();
-    this.state = {
-    };
-
+    this.state = {};
+    let accountId = this.props.data.accountId;
+    let eventType = this.props.data.selectedEventType;
+    let attribute = this.props.data.selectedAttribute;
+    let aggFunc = this.props.data.selectedAggFunc;
+    let scope = this.props.data.selectedScope;
+    let scopeOperator = this.props.data.selectedScopeOperator;
+    let facet = this.props.data.selectedFacet;
   }
-
 
   _onChange(event, value) {
     this.setState({ value });
   }
 
-  
+  buildNRQL() {
+    let nrql = [
+      "FROM ",
+      this.props.data.selectedEventType,
+      " ",
+      "SELECT ",
+      this.props.data.selectedAggFunc,
+      "(",
+      this.props.data.selectedAttribute,
+      ") ",
+      "WHERE ",
+      this.props.data.selectedScope,
+      " ",
+      this.props.data.selectedScopeOperator,
+      " Value ",
+      "FACET ",
+      this.props.data.selectedFacet,
+    ].join();
+
+    return nrql;
+  }
 
   render() {
+    let accountId = this.props.data.accountId;
+    let eventType = this.props.data.selectedEventType;
+    let attribute = this.props.data.selectedAttribute;
+    let aggFunc = this.props.data.selectedAggFunc;
+    let scope = this.props.data.selectedScope;
+    let scopeOperator = this.props.data.selectedScopeOperator;
+    let facet = this.props.data.selectedFacet;
 
-    let accountId = this.props.data.accountId
-    let eventType = this.props.data.selectedEventType
-    let attribute = this.props.data.selectedAttribute
-    let aggFunc = this.props.data.selectedAggFunc
-    let scope = this.props.data.selectedScope
-    let scopeOperator = this.props.data.selectedScopeOperator
-    let facet = this.props.data.selectedFacet
+    console.log(this.props.data.accountId);
 
-
-
-      console.log(this.props.data.accountId)
-    let styles = {
-      marginBottom: "10px",
-    };
-
+    let nrql = [
+        "FROM ",
+        this.props.data.selectedEventType,
+        " ",
+        "SELECT ",
+        this.props.data.selectedAggFunc,
+        "(",
+        this.props.data.selectedAttribute,
+        ") ",
+        "WHERE ",
+        this.props.data.selectedScope,
+        " ",
+        this.props.data.selectedScopeOperator,
+        " Value ",
+        "FACET ",
+        this.props.data.selectedFacet,
+      ].join();
+    
     return (
-      <div className="ChartPreview">
-          <BlockText>From {eventType} Select {aggFunc}({attribute}) where {scope} {scopeOperator} Facet {facet}</BlockText>
+      <div className="NRQLBlock">
+        <BlockText>
+            {nrql}
+        </BlockText>
       </div>
     );
   }
